ebb26f3821f70c68a2a4b0374f3c13c318ea7d30
Eli Craig <elidcraig@gmail.com>
2022-05-03 22:09:03 -0700

    in rendering of dog names, for...of loop changed to foreach

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(resp => resp.json())
     .then(doggoData => {
         const dogBar = document.querySelector('#dog-bar')
-        for(pup of doggoData) {
+        doggoData.forEach(pup => {
             const newSpanElement = document.createElement('span')
             newSpanElement.textContent = pup.name
             dogBar.appendChild(newSpanElement)
-        }
+        })        
     })
 })
+
+// When a user clicks on a pup's span in the div#dog-bar, that pup's info 
+// (image, name, and isGoodDog status) should show up in the div with the id of "dog-info". 
+
+// Display the pup's info in the div with the following elements:
+
+// an img tag with the pup's image url
+// an h2 with the pup's name
+// a button that says "Good Dog!" or "Bad Dog!" based on whether isGoodDog is true or false.
